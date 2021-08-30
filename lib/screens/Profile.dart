d7213521a5590bff5cbf7b3436b15e610da8bdef
Dragon Flip <62544653+dragonflip@users.noreply.github.com>
2021-08-30 15:01:30 +0300

    fix(profileScreen): crush when address not exists

--- lib/screens/Profile.dart
+++ lib/screens/Profile.dart
@@ -13,7 +13,7 @@ class Profile extends StatefulWidget {
   createState() => _Profile();
 }
 
-var futureData;
+var futureData, lastUid;
 
 class _Profile extends State<Profile> {
   var _editPhone = false;
@@ -23,14 +23,35 @@ class _Profile extends State<Profile> {
   Future getData() async {
     await getGlobals();
 
-    var profile = await http.get(Uri.parse('$apiUrl/user/$uid/pi'), headers: {'USERSID': sid});
+    lastUid = lastUid ?? uid;
 
-    return jsonDecode(utf8.decode(profile.bodyBytes));
+    var profileResponse = await http.get(Uri.parse('$apiUrl/user/$uid/pi'), headers: {'USERSID': sid});
+    var profile = jsonDecode(utf8.decode(profileResponse.bodyBytes));
+
+    profile['phone'] = profile['phone'].isNotEmpty ? profile['phone'][0] : '';
+    profile['email'] = profile['email'].isNotEmpty ? profile['email'][0] : '';
+
+    return profile;
+  }
+
+  Future checkData() async {
+    return getData().then((value) {
+      if (!mounted) return;
+
+      setState(() {
+        futureData = value;
+      });
+    });
   }
 
   @override
   void initState() {
-    futureData = futureData ?? getData();
+    if (lastUid != uid) {
+      futureData = null;
+      lastUid = uid;
+    }
+    checkData();
+
     super.initState();
   }
 
@@ -42,141 +63,126 @@ class _Profile extends State<Profile> {
         constraints: BoxConstraints.expand(),
         child: SingleChildScrollView(
           physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-          child: FutureBuilder(
-            future: futureData,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                final data = snapshot.data as Map<String, dynamic>;
+          child: futureData != null ? Card(
+            margin: EdgeInsets.all(8),
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text(futureData['fio']),
+                    subtitle: Text('ПІБ'),
+                    leading: Icon(Icons.person),
+                  ),
+                  ListTile(
+                    title: Text(login),
+                    subtitle: Text('Логін'),
+                    leading: Icon(Icons.assignment_ind),
+                  ),
+                  ListTile(
+                    title: _editPhone ? TextFormField(
+                      initialValue: futureData['phone'],
+                      cursorHeight: 22,
+                      focusNode: _focusNode,
 
-                return Card(
-                  margin: EdgeInsets.all(8),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 16, horizontal: 8),
-                    child: Column(
-                      children: [
-                        OutlinedButton.icon(
-                          onPressed: () {
-                            showDialog(
-                              context: context,
-                              builder: (_) => AlertDialog(
-                                title: Text('Заморозити аккаунт?'),
-                                content: Text('Замороження аккаунта призупиняє послуги та їх щомісячну оплату.'),
-                                actions: [
-                                  TextButton(
-                                    onPressed: () => Navigator.pop(context),
-                                    child: Text('СКАСУВАТИ', style: TextStyle(color: Colors.black54)),
-                                  ),
-                                  TextButton(
-                                    onPressed: () => Navigator.pop(context),
-                                    child: Text('ЗАМОРОЗИТИ'),
-                                  ),
-                                ],
-                              ),
-                            );
-                          },
-                          icon: Icon(Icons.block),
-                          label: Text('Заморозити аккаунт'),
-                        ),
-                        ListTile(
-                          title: Text(data['fio']),
-                          subtitle: Text('ПІБ'),
-                          leading: Icon(Icons.person),
-                        ),
-                        ListTile(
-                          title: Text(login),
-                          subtitle: Text('Логін'),
-                          leading: Icon(Icons.assignment_ind),
-                        ),
-                        ListTile(
-                          title: _editPhone ? TextFormField(
-                            initialValue: data['phone'][0],
-                            cursorHeight: 22,
-                            focusNode: _focusNode,
+                      decoration: InputDecoration(
+                        isDense: true,
+                        contentPadding: EdgeInsets.symmetric(vertical: 1),
+                      ),
 
-                            decoration: InputDecoration(
-                              isDense: true,
-                              contentPadding: EdgeInsets.symmetric(vertical: 1),
-                            ),
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (value) {
+                        if (value!.isEmpty) return 'Введіть телефон';
+                        futureData['phone'] = value;
+                      },
 
-                            validator: (value) {
-                              if (value!.isEmpty) {
-                                return 'Введіть телефон';
-                              }
+                    ) : futureData['phone'].isNotEmpty ? Text(futureData['phone']) : Text('Не вказано'),
+                    subtitle: Text('Телефон'),
+                    leading: Icon(Icons.phone),
+                    trailing: IconButton(
+                      onPressed: () async {
+                        if (_editPhone && futureData['phone'].isNotEmpty) {
+                        //   try {
+                        //     var res = await http.put(Uri.parse('$apiUrl/users/$uid/pi'), body: jsonEncode({'phone': futureData['phone'][0]}), headers: {'KEY': 'testAPI_KEY12'});
+                        //     print(res.body);
+                        //   } catch (e) {
+                        //     print(e);
+                        //   }
 
-                              data['phone'][0] = value;
-                            },
-                            autovalidateMode: AutovalidateMode.onUserInteraction,
-                          ) : Text(data['phone'][0]),
-                          subtitle: Text('Телефон'),
-                          leading: Icon(Icons.phone),
-                          trailing: IconButton(
-                            onPressed: () async {
-                              if (_editPhone) {
-                                try {
-                                  var res = await http.put(Uri.parse('$apiUrl/users/$uid/pi'), body: jsonEncode({'phone': data['phone'][0]}), headers: {'KEY': 'testAPI_KEY12'});
-                                  print(res.body);
-                                } catch (e) {
-                                  print(e);
-                                }
-                              }
+                          _focusNode.unfocus();
+                        } else {
+                          _focusNode.requestFocus();
+                        }
 
-                              setState(() {
-                                _editPhone = !_editPhone;
-                                _focusNode.requestFocus();
-                              });
-                            },
-                            icon: !_editPhone ? Icon(Icons.edit) : Icon(Icons.check, color: Colors.red),
-                            tooltip: !_editPhone ? 'Редагувати телефон' : 'Зберегти зміни',
-                          ),
-                        ),
-                        ListTile(
-                          title: Text(data['addressFull'].trim()),
-                          subtitle: Text('Адреса'),
-                          leading: Icon(Icons.home),
-                        ),
-                        ListTile(
-                          title: _editEmail ? TextFormField(
-                            initialValue: data['email'][0],
-                            cursorHeight: 22,
-                            focusNode: _focusNode,
-
-                            decoration: InputDecoration(
-                              isDense: true,
-                              contentPadding: EdgeInsets.symmetric(vertical: 1),
-                            ),
-                          ) : Text(data['email'][0]),
-                          subtitle: Text('Електронна пошта'),
-                          leading: Icon(Icons.email),
-                          trailing: IconButton(
-                            onPressed: () {
-                              setState(() {
-                                _editEmail = !_editEmail;
-                                _focusNode.requestFocus();
-                              });
-                            },
-                            icon: !_editEmail ? Icon(Icons.edit) : Icon(Icons.check, color: Colors.red),
-                            tooltip: !_editEmail ? 'Редагувати електронну пошту' : 'Зберегти зміни',
-                          ),
-                        ),
-                        ListTile(
-                          title: Text(data['contractId']),
-                          subtitle: Text('Контракт №'),
-                          leading: Icon(Icons.assignment),
-                        ),
-                        ListTile(
-                          title: Text(DateFormat('dd.MM.yyyy').format(DateTime.parse(data['contractDate']))),
-                          subtitle: Text('Дата підписання контракту'),
-                          leading: Icon(Icons.date_range),
-                        ),
-                      ],
+                        setState(() {
+                          _editPhone = !_editPhone;
+                        });
+                      },
+                      icon: !_editPhone ? Icon(Icons.edit) : Icon(Icons.check, color: Colors.red),
+                      tooltip: !_editPhone ? 'Редагувати телефон' : 'Зберегти зміни',
                     ),
                   ),
-                );
-              } else {
-                return LinearProgressIndicator();
-              }
-            },
-          ),
+                  ListTile(
+                    title: _editEmail ? TextFormField(
+                      initialValue: futureData['email'],
+                      cursorHeight: 22,
+                      focusNode: _focusNode,
+
+                      decoration: InputDecoration(
+                        isDense: true,
+                        contentPadding: EdgeInsets.symmetric(vertical: 1),
+                      ),
+
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (value) {
+                        if (value!.isEmpty) return 'Введіть телефон';
+                        futureData['email'] = value;
+                      },
+                    ) : futureData['email'].isNotEmpty ? Text(futureData['email']) : Text('Не вказано'),
+                    subtitle: Text('Електронна пошта'),
+                    leading: Icon(Icons.email),
+                    trailing: IconButton(
+                      onPressed: () async {
+                        if (_editEmail && futureData['email'].isNotEmpty) {
+                          // try {
+                          //   var res = await http.put(Uri.parse('$apiUrl/users/$uid/pi'), body: jsonEncode({'email': futureData['email']}), headers: {'KEY': 'testAPI_KEY12'});
+                          //   print(res.body);
+                          // } catch (e) {
+                          //   print(e);
+                          // }
+
+                          _focusNode.unfocus();
+                        } else {
+                          _focusNode.requestFocus();
+                        }
+
+                        setState(() {
+                          _editEmail = !_editEmail;
+                        });
+                      },
+                      icon: !_editEmail ? Icon(Icons.edit) : Icon(Icons.check, color: Colors.red),
+                      tooltip: !_editEmail ? 'Редагувати електронну пошту' : 'Зберегти зміни',
+                    ),
+                  ),
+                  ListTile(
+                    title: futureData.containsKey('addressFull') ? Text(futureData['addressFull'].trim()) : Text('Не вказано'),
+                    subtitle: Text('Адреса'),
+                    leading: Icon(Icons.home),
+                  ),
+                  ListTile(
+                    title: Text(futureData['contractId']),
+                    subtitle: Text('Контракт №'),
+                    leading: Icon(Icons.assignment),
+                  ),
+                  ListTile(
+                    title: Text(DateFormat('dd.MM.yyyy').format(DateTime.parse(futureData['contractDate']))),
+                    subtitle: Text('Дата підписання контракту'),
+                    leading: Icon(Icons.date_range),
+                  ),
+                ],
+              ),
+            ),
+          ) : LinearProgressIndicator(),
         ),
       ),
       bottomNavigationBar: MyBottomNavigationBar(),
