0c0c979e562afcc2f0ed93feb610554620887034
Dragon Flip <62544653+dragonflip@users.noreply.github.com>
2021-08-30 22:48:45 +0300

    fix(editProfile): delete because not needed in current time

--- lib/screens/Profile.dart
+++ lib/screens/Profile.dart
@@ -16,10 +16,6 @@ class Profile extends StatefulWidget {
 var futureData, lastUid;
 
 class _Profile extends State<Profile> {
-  var _editPhone = false;
-  var _editEmail = false;
-  final _focusNode = FocusNode();
-
   Future getData() async {
     await getGlobals();
 
@@ -28,8 +24,11 @@ class _Profile extends State<Profile> {
     var profileResponse = await http.get(Uri.parse('$apiUrl/user/$uid/pi'), headers: {'USERSID': sid});
     var profile = jsonDecode(utf8.decode(profileResponse.bodyBytes));
 
-    profile['phone'] = profile['phone'].isNotEmpty ? profile['phone'][0] : '';
-    profile['email'] = profile['email'].isNotEmpty ? profile['email'][0] : '';
+    print(profileResponse.body);
+
+    profile['phone'] = profile['phone'].isNotEmpty ? profile['phone'][0].trim() : 'Не вказано';
+    profile['email'] = profile['email'].isNotEmpty ? profile['email'][0].trim() : 'Не вказано';
+    profile['addressFull'] = profile.containsKey('addressFull') ? profile['addressFull'].trim() : 'Не вказано';
 
     return profile;
   }
@@ -80,92 +79,17 @@ class _Profile extends State<Profile> {
                     leading: Icon(Icons.assignment_ind),
                   ),
                   ListTile(
-                    title: _editPhone ? TextFormField(
-                      initialValue: futureData['phone'],
-                      cursorHeight: 22,
-                      focusNode: _focusNode,
-
-                      decoration: InputDecoration(
-                        isDense: true,
-                        contentPadding: EdgeInsets.symmetric(vertical: 1),
-                      ),
-
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      validator: (value) {
-                        if (value!.isEmpty) return 'Введіть телефон';
-                        futureData['phone'] = value;
-                      },
-
-                    ) : futureData['phone'].isNotEmpty ? Text(futureData['phone']) : Text('Не вказано'),
+                    title: Text(futureData['phone']),
                     subtitle: Text('Телефон'),
                     leading: Icon(Icons.phone),
-                    trailing: IconButton(
-                      onPressed: () async {
-                        if (_editPhone && futureData['phone'].isNotEmpty) {
-                        //   try {
-                        //     var res = await http.put(Uri.parse('$apiUrl/users/$uid/pi'), body: jsonEncode({'phone': futureData['phone'][0]}), headers: {'KEY': 'testAPI_KEY12'});
-                        //     print(res.body);
-                        //   } catch (e) {
-                        //     print(e);
-                        //   }
-
-                          _focusNode.unfocus();
-                        } else {
-                          _focusNode.requestFocus();
-                        }
-
-                        setState(() {
-                          _editPhone = !_editPhone;
-                        });
-                      },
-                      icon: !_editPhone ? Icon(Icons.edit) : Icon(Icons.check, color: Colors.red),
-                      tooltip: !_editPhone ? 'Редагувати телефон' : 'Зберегти зміни',
-                    ),
                   ),
                   ListTile(
-                    title: _editEmail ? TextFormField(
-                      initialValue: futureData['email'],
-                      cursorHeight: 22,
-                      focusNode: _focusNode,
-
-                      decoration: InputDecoration(
-                        isDense: true,
-                        contentPadding: EdgeInsets.symmetric(vertical: 1),
-                      ),
-
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      validator: (value) {
-                        if (value!.isEmpty) return 'Введіть телефон';
-                        futureData['email'] = value;
-                      },
-                    ) : futureData['email'].isNotEmpty ? Text(futureData['email']) : Text('Не вказано'),
+                    title: Text(futureData['email']),
                     subtitle: Text('Електронна пошта'),
                     leading: Icon(Icons.email),
-                    trailing: IconButton(
-                      onPressed: () async {
-                        if (_editEmail && futureData['email'].isNotEmpty) {
-                          // try {
-                          //   var res = await http.put(Uri.parse('$apiUrl/users/$uid/pi'), body: jsonEncode({'email': futureData['email']}), headers: {'KEY': 'testAPI_KEY12'});
-                          //   print(res.body);
-                          // } catch (e) {
-                          //   print(e);
-                          // }
-
-                          _focusNode.unfocus();
-                        } else {
-                          _focusNode.requestFocus();
-                        }
-
-                        setState(() {
-                          _editEmail = !_editEmail;
-                        });
-                      },
-                      icon: !_editEmail ? Icon(Icons.edit) : Icon(Icons.check, color: Colors.red),
-                      tooltip: !_editEmail ? 'Редагувати електронну пошту' : 'Зберегти зміни',
-                    ),
                   ),
                   ListTile(
-                    title: futureData.containsKey('addressFull') ? Text(futureData['addressFull'].trim()) : Text('Не вказано'),
+                    title: Text(futureData['addressFull']),
                     subtitle: Text('Адреса'),
                     leading: Icon(Icons.home),
                   ),
